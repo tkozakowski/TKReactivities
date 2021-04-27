7d2309f9a4c6f3287f79fba1c03b8a60e92f7564
Tomasz Kozakowski <tkozakowski@gmail.com>
2021-04-27 23:41:45 +0200

    Medaitor create, read, update

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,38 @@
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FirstOrDefaultAsync(a => a.Id == request.Activity.Id);
+
+                _mapper.Map(request.Activity, activity);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
