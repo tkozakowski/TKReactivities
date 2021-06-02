4c681ac98509813bcee2b84e646794a01aadd00e
Tomek <t>
2021-06-02 15:50:16 +0200

    SignalRChatHub

---
+++ API/SignalR/ChatHub.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Application.Comments;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    public class ChatHub : Hub
+    {
+        private readonly IMediator _mediator;
+
+        public ChatHub(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task SendComment(Create.Command command)
+        {
+            var comment = await _mediator.Send(command);
+
+            await Clients.Group(command.ActivityId.ToString())
+                .SendAsync("ReceiveComment", comment.Value);
+        }
+    }
+}
